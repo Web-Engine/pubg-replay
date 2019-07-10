51aea65649ddd05c2c0448b0038d78063b5650dd
TaeSang Cho <gsts007@gmail.com>
2019-07-10 20:51:59 +0900

    Configure eslint

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+let array = [
+    [ 1, 2, 3 ],
+];
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+    array,
+}).$mount('#app');
