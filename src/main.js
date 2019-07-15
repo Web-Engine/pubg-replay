908b0359ce2ddb876e138c43edc78394f80ead96
TaeSang Cho <gsts007@gmail.com>
2019-07-15 13:16:53 +0900

    Add default store

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import Vue from 'vue';
-import Vuex from 'vuex'
-import VueRouter from 'vue-router'
+import Vuex from 'vuex';
 
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuex);
-Vue.use(VueRouter);
 
 let array = [
     [ 1, 2, 3 ],
 ];
 
+import store from './store/index';
+
 new Vue({
     render: h => h(App),
     array,
+    store,
 }).$mount('#app');
