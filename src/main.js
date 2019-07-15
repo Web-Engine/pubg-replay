7b54e1f2286f51782a9049270954459c402d76e8
TaeSang Cho <gsts007@gmail.com>
2019-07-15 13:09:42 +0900

    Initialize vue-router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuex);
+Vue.use(VueRouter);
 
 let array = [
     [ 1, 2, 3 ],
