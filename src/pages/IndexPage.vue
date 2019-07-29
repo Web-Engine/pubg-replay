98a024d8b3f099cde2b997f550a9ba780a3003ea
TaeSang Cho <gsts007@gmail.com>
2019-07-29 17:22:56 +0900

    Use custom select component on index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,49 @@
 <template>
     <div class="IndexPage">
-        Index
+        <form>
+            <Select name="platform" title="Platform" :options="platforms"></Select>
+            <input type="text" name="player-name" placeholder="Player Name">
+
+            <button type="submit">SEARCH</button>
+        </form>
     </div>
 </template>
 
 <script>
+import Select from './../components/forms/Select.vue';
+
 export default {
     name: 'IndexPage',
+    components: { Select },
     data() {
-        return {};
+        return {
+            platforms: [
+                {
+                    value: 'steam',
+                    text: 'STEAM',
+                },
+                {
+                    value: 'xbox',
+                    text: 'XBOX',
+                },
+                {
+                    value: 'kakao',
+                    text: 'KAKAO',
+                },
+            ],
+        };
     },
 };
 </script>
 
 <style scoped>
-.IndexPage {
+    .IndexPage {
+        padding: 300px 0;
+    }
 
-}
+    .IndexPage form {
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        padding: 30px;
+    }
 </style>
