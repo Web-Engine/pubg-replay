da3f4cb9d3dcdf9aa24c6908a8ee1ee84cae34bd
azealia <jinsu2504@naver.com>
2019-07-29 22:31:02 +0900

    Add router to import a match by the player name

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.get('/player/:name', async (req, res, next) => {
 
+    let response = await request.get({
+        url: `https://api.pubg.com/shards/steam/players?filter[playerNames]=${req.params.name}`,
+        headers: {
+            'Authorization': '',
+            'Accept': 'application/json',
+        },
+    });
+
+});
+
+const server = app.listen(8080, () => {
+    console.log('Server started at port', server.address().port);
 });
 
 
