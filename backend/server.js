dbfaf27109b5e4f7d5d812c0b246aa32e9edb7c7
azealia <jinsu2504@naver.com>
2019-07-29 22:55:13 +0900

    Update server.js
    *Add data response
    *Add api key

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 const request = require('request-promise-native');
 const express = require('express');
 const app = express();
+const apikey = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5NTdhZTMzMC05NDBiLTAxMzctY2NhZi0wMTlhZmY3MjIwZWIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTY0MzkwMTE5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImppbnN1MjUwNC1uYXZlIn0.Zpcy7YdOvjzuN2889h0YMwoqRfpgiFH4558CvQJeqHo';
 
 app.get('/player/:name', async (req, res, next) => {
 
     let response = await request.get({
         url: `https://api.pubg.com/shards/steam/players?filter[playerNames]=${req.params.name}`,
         headers: {
-            'Authorization': '',
+            'Authorization': apikey,
             'Accept': 'application/json',
-        },
+        }
     });
+
+    res.send(JSON.parse(response));
 
 });
 
@@ -19,7 +22,7 @@ app.get('/matches/:id', async (req, res) => {
     let response = await request.get({
         url: `https://api.pubg.com/shards/steam/matches/${req.params.id}`,
         headers: {
-            'Authorization': '',
+            'Authorization': apikey,
             'Accept': 'application/json',
         },
     });
@@ -33,11 +36,13 @@ app.get('/matches/:id', async (req, res) => {
     response = await request.get({
         url:  telementryURL,
         headers: {
-            'Authorization': '',
+            'Authorization': apikey,
             'Accept': 'application/json',
         },
         gzip: true,
     });
+
+    res.send(JSON.parse(response));
 });
 
 const server = app.listen(8080, () => {
