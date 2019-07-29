348858410f58aa9aeeb915eb0270bb418f7b2ddb
TaeSang Cho <gsts007@gmail.com>
2019-07-29 17:23:16 +0900

    Add replay page

---
+++ src/pages/ReplayPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="ReplayPage">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ReplayPage',
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style scoped>
+    .ReplayPage {
+    }
+</style>
