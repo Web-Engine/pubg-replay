48b410fad23ae08e36c31089628ad3ab873f8695
seong min Kim <skattr13@naver.com>
2019-07-30 14:08:32 +0900

    Add and design the player name box

--- frontend/src/pages/MatchesPage.vue
+++ frontend/src/pages/MatchesPage.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="MatchesPage">
+        <div class="playerName">
+                player name
+        </div>
     </div>
 </template>
 
@@ -13,4 +16,17 @@ export default {
 </script>
 
 <style scoped>
+    .MatchesPage {
+    }
+
+    .MatchesPage .playerName {
+        box-sizing: border-box;
+        margin: 10px 5px;
+        height: 120px;
+        font-size: 50px;
+        text-align: center;
+        background: #a6a6a6;
+        color: white;
+        line-height: 120px;
+    }
 </style>
