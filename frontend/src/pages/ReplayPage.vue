3f56ea1d2d6ec79ce65270df3ae384bd7570ab3b
seong min Kim <skattr13@naver.com>
2019-07-30 15:02:29 +0900

    Add the ui box, the replay box and the kill log box

--- frontend/src/pages/ReplayPage.vue
+++ frontend/src/pages/ReplayPage.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="ReplayPage">
-        asdfghhdk
+        <div class="ui">
+            ui
+        </div>
+        <div class="clearfix">
+            <div class="replay fleft">
+                replay
+            </div>
+            <div class="killLog fleft">
+                kill log
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,6 +24,17 @@ export default {
 </script>
 
 <style scoped>
+    .fleft {
+        float: left;
+    }
+
+    .clearfix::after {
+        display: block;
+        content: "";
+        clear: both;
+    }
+
     .ReplayPage {
+        margin: 50px 100px;
     }
 </style>
