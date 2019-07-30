bfd52d2496cc40bbe30aeb261030d3fe62084f48
seong min Kim <skattr13@naver.com>
2019-07-30 14:13:12 +0900

    Add and design the matches box

--- frontend/src/pages/MatchesPage.vue
+++ frontend/src/pages/MatchesPage.vue
@@ -3,6 +3,39 @@
         <div class="playerName">
                 player name
         </div>
+        <div class="matchs clearfix">
+            <div class="solo fleft">
+                <div class="text" id="soloText">
+                    <img src="../../images/User.png">
+                    SOLO
+                </div>
+                <div class="data" id="soloData">
+                    This player doesn't have a solo match
+                </div>
+            </div>
+            <div class="duo fleft">
+                <div class="text" id="duoText">
+                    <img src="../../images/User.png">
+                    <img src="../../images/User.png">
+                    DUO
+                </div>
+                <div class="data" id="duoData">
+                    This player doesn't have a duo match
+                </div>
+            </div>
+            <div class="squad fleft">
+                <div class="text" id="squadText">
+                    <img src="../../images/User.png">
+                    <img src="../../images/User.png">
+                    <img src="../../images/User.png">
+                    <img src="../../images/User.png">
+                    SQUAD
+                </div>
+                <div class="data" id="squadData">
+                    This player doesn't have a squad match
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,6 +49,16 @@ export default {
 </script>
 
 <style scoped>
+    .fleft {
+        float: left;
+    }
+
+    .clearfix {
+        display: block;
+        content: '';
+        clear: both;
+    }
+
     .MatchesPage {
     }
 
@@ -28,5 +71,57 @@ export default {
         background: #a6a6a6;
         color: white;
         line-height: 120px;
+    }
+
+    .matchs {
+    }
+
+    .matchs .text {
+        box-sizing: border-box;
+        margin: 0 5px;
+        padding-left: 10px;
+        width: 390px;
+        height: 40px;
+        font-size: 20px;
+        line-height: 40px;
+        color: white;
+    }
+
+    .matchs .text img {
+        width: 20px;
+        height: 20px;
+    }
+
+    .matchs #soloText {
+        background: #ff9514;
+    }
+
+    .matchs #duoText {
+        background: #16de59;
+    }
+
+    .matchs #squadText {
+        background: #1979ff;
+    }
+
+    .matchs .data {
+        box-sizing: border-box;
+        margin: 0 5px 10px;
+        padding: 10px;
+        width: 390px;
+        text-align: center;
+        color: white;
+    }
+
+    .matchs #soloData {
+        background: #ffbc75;
+    }
+
+    .matchs #duoData {
+        background: #5be383;
+    }
+
+    .matchs #squadData {
+        background: #77b7f7;
     }
 </style>
