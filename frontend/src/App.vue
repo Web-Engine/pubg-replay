ce68875bfb4eda89aef848667e03cf24b8e54c2d
TaeSang Cho <gsts007@gmail.com>
2019-08-06 16:52:41 +0900

    Add time and speed slider

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,47 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title>Replay</v-toolbar-title>
+        </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        <v-content>
+            <v-slider min="0" max="3302123" thumb-size="60">
+                <template #thumb-label="currentTime">
+                    {{ timeFormat(currentTime.value) }}
+                </template>
+            </v-slider>
+
+            <v-slider min="1" max="30" thumb-size="40">
+                <template #thumb-label="speed">
+                    x{{ speed.value }}
+                </template>
+            </v-slider>
+
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
     name: 'App',
-    components: {
-        HelloWorld,
+    components: {},
+    methods: {
+        timeFormat: time => {
+            let milliseconds = time % 1000;
+            let seconds = Math.floor(time / 1000);
+
+            let minutes = Math.floor(seconds / 60);
+            seconds %= 60;
+
+            milliseconds = Math.floor(milliseconds / 100);
+            seconds = seconds.toString().padStart(2, '0');
+            minutes = minutes.toString().padStart(2, '0');
+
+            return `${minutes}:${seconds}.${milliseconds}`;
+        },
     },
     data: () => ({
-    //
+
     }),
 };
 </script>
